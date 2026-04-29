e6f2418b266c118da000988885d13825307c3d91
Induja22 <indujavelusamyp@gmail.com>
2026-04-29 21:25:23 +0530

    Using do while loop get input and terminate if input is 0

---
+++ userinput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class userinput{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n;
+        do{
+            n=s.nextInt();
+            System.out.println("The number is: " + n);
+        } while(n != 0);
+    }
+}
