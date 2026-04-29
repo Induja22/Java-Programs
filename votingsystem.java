e4d79ee4448743725247194e4c3c005d30565149
Induja22 <indujavelusamyp@gmail.com>
2026-04-29 11:29:15 +0530

    Voting system using if-else statement
    Signed-off-by: Induja22 <indujavelusamyp@gmail.com>

---
+++ votingsystem.java
@@ -0,0 +1,10 @@
+class votingsystem{
+    public static void main(String args[]){
+        int age = 10;
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
