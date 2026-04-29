3992a2f0e6b0f9bff0414517d7362fcea166fc19
Induja22 <indujavelusamyp@gmail.com>
2026-04-29 11:19:47 +0530

    Score Grading System
    Signed-off-by: Induja22 <indujavelusamyp@gmail.com>

---
+++ score.java
@@ -0,0 +1,19 @@
+class score{
+    public static void main(String[] args) {
+        int score = 85;
+        System.out.println("Score : " + score);
+        if(score >= 90) {
+            System.out.println("Grade: A+");
+        } else if(score >= 80) {
+            System.out.println("Grade: A");
+        } else if(score >= 70) {
+            System.out.println("Grade: B+");
+        } else if(score >= 60) {
+            System.out.println("Grade: B");
+        } else if(score >= 50) {
+            System.out.println("Grade: C");
+        } else{
+            System.out.println("Grade: Fail(U)");
+        }
+    }
+}
