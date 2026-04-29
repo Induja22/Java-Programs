259bfe4e43f56fd216c015e0ffce1c6bed4549ee
Induja22 <indujavelusamyp@gmail.com>
2026-04-29 21:49:16 +0530

    Compare when the string is equal
    Signed-off-by: Induja22 <indujavelusamyp@gmail.com>

---
+++ stringcompare.java
@@ -0,0 +1,9 @@
+class stringcompare{
+    public static void main(String arr[]) {
+        String s1="abc";
+        String a = new String("abc");
+        System.out.println(a);
+        System.out.println(s1==a);
+        System.out.println(s1.equals(a));
+    }
+}
