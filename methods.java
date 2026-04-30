49731836dd926beb6c58f79e9f51fde9bab3a408
Induja22 <indujavelusamyp@gmail.com>
2026-04-30 16:09:39 +0530

    Non static method using class

---
+++ methods.java
@@ -0,0 +1,11 @@
+class methods{
+    public static void main(String[] args) {
+        method obj = new method();
+        obj.meth();
+    }
+}
+class method{
+    void meth(){
+        System.out.println("Hi");
+    }
+}
