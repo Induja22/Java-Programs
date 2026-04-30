b4a65273b12c5b48b42da1f12597887db82f5d67
Induja22 <indujavelusamyp@gmail.com>
2026-04-30 12:23:24 +0530

    Character Check (Vowel or Consonant)
    Signed-off-by: Induja22 <indujavelusamyp@gmail.com>

--- task1.java
+++ task1.java
@@ -5,7 +5,8 @@ class task1{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
         char ch = sc.next().charAt(0);
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+        ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
             System.out.println("Vowel");
         } else {
             System.out.println("Consonant");
