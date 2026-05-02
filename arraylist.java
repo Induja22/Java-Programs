3fb4b4e5b927f57beef2214ba1fa83ecf228eb44
Induja22 <indujavelusamyp@gmail.com>
2026-05-02 12:04:10 +0530

    Basics of ArrayList: Creation,Add and Remove element,sort,Contains
    Signed-off-by: Induja22 <indujavelusamyp@gmail.com>

--- arraylist.java
+++ arraylist.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.ArrayList;
 class arraylist {
     public static void main(String[] args) {
@@ -8,10 +9,13 @@ class arraylist {
         arr.add(40);
         arr.add(50);
         System.out.println(arr);
-        arr.set(3,100);   //arr.set(index,value)
+        arr.set(3,10);   //arr.set(index,value)
         arr.set(4,20);
         System.out.println(arr);
         arr.remove(4);     //arr.remove(index)
         System.out.println(arr);
+        System.out.println(arr.contains(2));   //arr.contains(value)
+        Collections.sort(arr);
+        System.out.println(arr);  
     }
 }
